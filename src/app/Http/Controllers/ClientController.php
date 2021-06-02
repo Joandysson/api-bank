e7c092731870310a96f2ab1da4e8127c4e8cda93
Joandysson Gama <joandysson.gama@gmail.com>
2021-06-01 23:35:05 -0300

    init controller

--- src/app/Http/Controllers/ClientController.php
+++ src/app/Http/Controllers/ClientController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Client;
-
+use Laravel\Lumen\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use PhpParser\Node\Stmt\TryCatch;
 
 class ClientController extends Controller
 {
@@ -12,5 +14,38 @@ class ClientController extends Controller
     public function __construct(Client $client)
     {
         $this->client = $client;
+    }
+
+    function index(Request $request)
+    {
+    }
+
+    function create(Request $request)
+    {
+        try {
+            $data = $request->all();
+            $this->client::create($request->all());
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
+
+        Hash::make();
+        Hash::check('', '');
+
+    }
+
+    function update(Request $request)
+    {
+
+    }
+
+    function show(Request $request)
+    {
+
+    }
+
+    function delete(Request $request)
+    {
+
     }
 }
